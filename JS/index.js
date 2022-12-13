9732c1b89e7492fafbd8e38263a257655d279806
AlexTanui <tanuialex417@gmail.com>
2022-12-13 14:34:40 +0300

    updated Readme

--- JS/index.js
+++ JS/index.js
@@ -1,26 +1,27 @@
-const url= "https://api.npoint.io/38aebdb16c6ba20c7efb/films/"
+const url = "https://api.npoint.io/38aebdb16c6ba20c7efb/films/"
 let info = document.getElementById("append")
 let leftList = document.getElementById("list")
 const searchBtn1 = document.getElementById("searchBtn")
-const input =document.getElementById("search").value
+const input = document.getElementById("search").value
 
 searchBtn1.addEventListener('click', flatMovies)
 
-
+// fetch for displaying movies and description on the dom 
 fetch(`${url}${input}`).then((response) => response.json())
-.then(json=>{json.map(data=>
-   
-    {console.log(data)
-        info.append(flatMovies(data))
-    
-    })})
-    
+    .then(json => {
+        json.map(data => {
+            console.log(data)
+            info.append(flatMovies(data))
+
+        })
+    })
+
 // funtion to print movies on dom
- 
-function flatMovies({title,runtime, showtime, description,poster,tickets_sold,id}) {
-  
-    let details =document.createElement("container")
-   details.innerHTML =`
+
+function flatMovies({ title, runtime, showtime, description, poster, tickets_sold, id }) {
+
+    let details = document.createElement("container")
+    details.innerHTML = `
                         <div id="append" class="movie-result">
                         
                                 <div class="movie-item ">
@@ -33,61 +34,53 @@ function flatMovies({title,runtime, showtime, description,poster,tickets_sold,id
                                     <p class="card-text">Runtime:  ${runtime}</p>
                                     <p class="card-text">Show Time:  ${showtime}</p>
                                     <p>Remaining tickets<p>
-                                    <p id="span"><u>${tickets_sold}</u></p>
-                                    <button onclick="totalClick(-1)" id="tickets${id}" type="button" class="btn btn-danger">Get ticket</button>
+                                    <p id="span${id}"><u>${tickets_sold}</u></p>
+                                    <button onclick="totalClick(-1, ${id})" id="tickets${id}" type="button" class="btn btn-danger">Get ticket</button>
                                 </div>
                                 </div>
                                   
                          </div>
     `;
- 
+
 
 
     return details
 }
-// function for list
+// function for list on the left side 
 
 fetch(`${url}`).then((response) => response.json())
-.then(json=>{json.map(data=>
-   
-    {console.log(data)
-    list.append(flatList(data))
-    
-    })})
-function flatList({title}) {
+    .then(json => {
+        json.map(data => {
+            console.log(data)
+            list.append(flatList(data))
+
+        })
+    })
+function flatList({ title }) {
     let list = document.createElement("ol")
-   list.innerHTML =`
+    list.innerHTML = `
  
-   <ul class="list-group">
+   <ol class="list-group">
                 <li class="list-group-item mt-2">${title}</li>
                 
-              </ul>
-        
+              </ol>
+          
         
     `;
     return list
 }
 flatList()
 
-// function vote({tickets_sold}){
-//     let count =parseInt(tickets_sold)
-//     count.innerHTML = tickets_sold -1
-// if(count <=0){
-//     alert("ticket  diplited")
-// }
-// return count
-// }
-// vote()
-
-function totalClick(clicks){
-    const totalClicks= document.getElementById("span")
+// function to reduce the number of tickets once sold 
+function totalClick(clicks, id) {
+    const totalClicks = document.getElementById(`span${id}`)
     const sumvalue = parseInt(totalClicks.innerText) + clicks
     totalClicks.innerText = sumvalue
-    
-    if(sumvalue < 0){
+
+    if (sumvalue < 0) {
         totalClicks.innerText = 0
         alert("sorry tickets diplited")
     }
     return totalClicks
-     }
-    totalClick()
+}
+totalClick()
---
+++ README.md
@@ -0,0 +1,95 @@
+# FlatDjango
+
+## Discription
+
+This is an application that allowsa a user to buy ticket for feveroutite movie it uses a local API and build out the frontend.
+The challenge was used to test our knowledge in JavaScript. The test included , DOM manipulation, arrays and using fetch method. 
+
+## Built with
+This application was built with the following technologies:
+
+    *HTML
+    *CSS
+    *JavaScript
+    *Bootstrap
+
+## Running The Live Hosted Application
+
+* open this [link](https://flatacutieschallenge.netlify.app/) on your browser to access the live web application
+* you can also follow the link on the left side of the repository below the discription
+* on the browser there are five dropdown link to individual animal which i did for privacy for votes
+* click on the link and get a ticket for the chosen movie.Once the tickets are depleted a user cannot access the movie
+
+## Installation
+
+* In order for you to use the content on this repo ensure you have the following:
+
+A computer that runs on either of the following; (Windows 7+, Linux, Mac OS)
+nodejs 9.0+
+
+* To intall this challengeor use in your computer you can either ``clone`` or ``fork`` the project
+Open a terminal / command line interface on your computer
+
+* Clone the repo by using the following to create a copy on your local machine:
+
+ `` git clone https://https://github.com/AlexTanui/week-one-code-challenge``
+* Change directory to the repo folder:
+
+ `` cd /mnt/d..``
+ 
+*  Open it in Visual Studio Code
+
+  ``code .``
+
+## Usage
+
+to use and test the code you can run
+
+``npm index.js ``
+
+``npm start``
+
+* To get the backend started you need to run
+
+  ``json-server --watch db.json``
+
+ * you can test your  server by visiting this route in the browser:
+ 
+  ``http://localhost:3000/``  or from n-point ``https://api.npoint.io/38aebdb16c6ba20c7efb/films/``
+  
+* Find the HTML file you want to view, right-click on it, and choose Open with from the menu. You will see a full list of apps that you can use to run your file. Your default browser will be at the top of the list.
+* Select Google Chrome from the list, and view your file in the browser.
+* on the browser, click ``get ticket`` to purchase a movie ticket
+
+
+## Contributing
+
+Please refer to each project's style and contribution guidelines for submitting patches and additions. In general, we follow the "fork-and-pull" Git workflow.
+
+[Fork](https://github.com/AlexTanui/Flatacuties-) the repo on GitHub and 
+
+clone   ``git clone https://github.com/AlexTanui/Flatiron-movie `` 
+
+to set the project to your own machine
+* Commit changes to your own branch
+* Push your work back up to your fork
+* Submit a Pull request so that we can review your changes
+NOTE: Be sure to merge the latest from "upstream" before making a pull request!
+
+## Issues
+
+Feel free to submit issues and enhancement requests.
+
+Please use specific bugs and errors.
+
+## License
+
+[IST](https://opensource.org/licenses/ISC)
+
+
+## Author
+
+[Alex Tanui](https://github.com/AlexTanui)
+
+
+Happy coding😎 
